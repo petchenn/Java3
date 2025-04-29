c8d5a190ed807c165ea4b9ea6c0f1712971450ea
petchenn <alya030305z@gmail.com>
2025-04-29 22:30:20 +0300

    Добавление JavaDoc для CompareLists

--- src/CompareLists.java
+++ src/CompareLists.java
@@ -1,8 +1,15 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+ * Этот класс измеряет время выполнения основных операций с классоми ArrayList и LinkedList,
+ * таких, как добавление, получение и удаление. Использует TestLists
+ */
 public class CompareLists {
     private TestLists testThisLists = new TestLists();
+    /**
+     * Основной метод для выполнения тестов производительности. Дважды вызывает класс TestLists.
+     */
     public CompareLists(){
         System.out.println("ArrayList: ");
         testThisLists.testList(new ArrayList<>());
